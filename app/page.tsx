1525b7225cbe2d209b43f29942312df3f9a3b449
thuyein1996alpha <thuyein1996alpha@gmail.com>
2025-06-17 15:38:36 +0000

    add styling to recipe list page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "@/lib/firebase" 
 import Link from 'next/link';
+import styles from './RecipeList.module.css';
 
 async function getRecipes() {
   const recipesCollection = collection(db, "recipes");
@@ -16,11 +17,11 @@ export default async function Home() {
   const recipes = await getRecipes();
 
   return (
-    <div>
+    <div className="container">
       <h1>Recipe List</h1>
       <ul>
         {recipes.map(recipe => (
-          <li key={recipe.id}>
+          <li key={recipe.id} className={styles.recipeItem}>
             {/* Wrap the recipe name in a Link component */}
             <Link href={`/recipes/${recipe.id}`}>
               <h2>{recipe.name}</h2>
